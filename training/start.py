93bab3a0c5b791e5062049f19f9433c1784c25ce
Jonas Klotz <jonasklotz@hotmail.de>
2021-07-15 13:49:13 +0200

    added start skript for aws

---
+++ training/start.py
@@ -0,0 +1,7 @@
+from training import mobile_nasnet, mobilenet_v2, xception, resnet_v2
+
+if __name__ == '__main__':
+    mobilenet_v2.start_training()
+    mobile_nasnet.start_training()
+    xception.start_training()
+    resnet_v2.start_training()
