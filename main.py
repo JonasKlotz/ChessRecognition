a4260f8c854bb1f06b8b394b838c72bb07c59b99
Jonas Klotz <jonasklotz@hotmail.de>
2021-09-20 10:03:09 +0200

    added scaling factor for board splitting, organized board processing in single file

--- main.py
+++ main.py
@@ -3,29 +3,30 @@
 import argparse
 import time
 
-import get_slid
-import model
 # do some stuff
-import utility
+import model
 from calculate_fen.get_fen import get_fen_from_predictions
-
+from detectboard import get_slid
 ##### Model Specific Parameters
-# from keras.applications.inception_resnet_v2 import preprocess_input
-# from keras.applications.resnet_v2 import preprocess_input
+from process_board import process_board
 
-"""
-# Mobile Net V2
-from keras.applications.mobilenet_v2 import preprocess_input
-num_of_classes=13
-img_size = 224
-model_path = '/home/joking/PycharmProjects/Chess_Recognition/models/13_class_MobileNetV2/model.h5'
-"""
-# Resnet V2
+"""# from keras.applications.inception_resnet_v2 import preprocess_input
+# from keras.applications.resnet_v2 import preprocess_input
+from process_board import double_split_board, process_board"""
+
+"""# Resnet V2
 from keras.applications.inception_resnet_v2 import preprocess_input
 
 num_of_classes = 7
 img_size = 150
 model_path = '/home/joking/PycharmProjects/Chess_Recognition/models/InceptionResNetV2/model.h5'
+"""
+
+from keras.applications.mobilenet_v2 import preprocess_input
+
+num_of_classes = 13
+img_size = 224
+model_path = "/home/joking/Projects/Chessrecognition/models/13_classes/final_MobileNetV2.h5"
 
 
 def process_image(path, save=False):
@@ -35,24 +36,11 @@ def process_image(path, save=False):
     # squares, board_img = get_board.get_squares(cropped, show=False)  #
     # print("Anzahl gefundene Squares ", len(squares))
 
-    squares, board_img, corners = get_slid.get_board_slid(path)
+    board_img, corners = get_slid.get_board_slid(path)
 
-
-    if save:
-        save_path = utility.fill_dir_with_squares(path, squares)
-        print("Saved to '%s'" % save_path)
-        # tensor_list, square_list = utility.load_square_lists_from_dir(save_path)
-
-    tensor_list = utility.load_tensor_list_from_squares(squares, img_size, preprocess_input)
-
-    elapsed_time = time.process_time() - start_time
-    print("Processing took ", elapsed_time, "seconds...")
-
-    # get Predictions
     start_time = time.process_time()
     reloaded_model = model.load_compiled_model(model_path)
-    predictions = model.get_predictions(reloaded_model, tensor_list)
-
+    predictions, squares = process_board(board_img, reloaded_model, img_size, preprocess_input)
     elapsed_time = time.process_time() - start_time
     print("Model took ", elapsed_time, "seconds...")
 
@@ -61,7 +49,9 @@ def process_image(path, save=False):
     fen = get_fen_from_predictions(predictions, squares, num_of_classes=num_of_classes)
     elapsed_time = time.process_time() - start_time
     print("Get Fen took ", elapsed_time, "seconds...")
+    print(fen)
     return fen
+
 
 if __name__ == '__main__':
     # Create the parser
@@ -85,6 +75,6 @@ if __name__ == '__main__':
     save = args.save
 
     # print(vars(args))
-    path = "data/chessboards/32.jpg"
+    path = "data/chessboards/1.jpg"
     print("Loading board from ", path)
     process_image(path, save=save)
